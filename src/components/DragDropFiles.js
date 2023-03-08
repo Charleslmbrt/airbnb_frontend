7e0aa9b377f654cdbd4242e35465a4de14114504
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2023-03-08 20:16:52 +0100

    add drag and drop files + description textarea

---
+++ src/components/DragDropFiles.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+
+const fileTypes = ["JPG", "PNG", "GIF", "WEBP", "JPEG"];
+
+const DragDropFiles = () => {
+  const [file, setFile] = useState(null);
+  const handleChange = (file) => {
+    setFile(file);
+  };
+  return (
+    <>
+      <FileUploader
+        handleChange={handleChange}
+        name="file"
+        types={fileTypes}
+        value={file}
+      />
+    </>
+  );
+};
+
+export default DragDropFiles;
--- src/pages/Publish.js
+++ src/pages/Publish.js
@@ -2,26 +2,50 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import PlacesAutocomplete, {
   geocodeByAddress,
-  geocodeByPlaceId,
+  // geocodeByPlaceId,
   getLatLng,
 } from "react-places-autocomplete";
+import { FileUploader } from "react-drag-drop-files";
 
 // imports assets
 import homeAirbnbVideo from "../img/video_home_airbnb.mp4";
 import logoAirbnb from "../img/logo-airbnb.svg";
 
+//import components
+// import DragDropFiles from "../components/DragDropFiles";
+
 const Publish = () => {
+  const [placeType, setPlaceType] = useState("");
   const [address, setAddress] = useState("");
   const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
   const [guestCounter, setGuestCounter] = useState(0);
   const [bedroomsCounter, setBedroomsCounter] = useState(0);
+  const [bedsCounter, setBedsCounter] = useState(0);
+  const [bathroomsCounter, setBathroomsCounter] = useState(0);
+  const [amenities, setAmenities] = useState("");
+
+  const [describe, setDescribe] = useState("");
+  const maxLength = 50;
+  const handleDescribe = (e) => {
+    const textAreaValue = e.target.value;
+    if (textAreaValue.length <= maxLength) {
+      setDescribe(textAreaValue);
+    }
+  };
+
+  const [file, setFile] = useState(null);
 
   const handleSelect = async (value) => {
     const addressResult = await geocodeByAddress(value);
     const coordinatesResult = await getLatLng(addressResult[0]);
-    console.log(coordinatesResult);
-    setAddress(addressResult);
+    setAddress(value);
     setCoordinates(coordinatesResult);
+  };
+
+  const fileTypes = ["JPG", "PNG", "GIF", "WEBP", "JPEG"];
+
+  const handleChange = (file) => {
+    setFile(file);
   };
 
   return (
@@ -58,9 +82,9 @@ const Publish = () => {
           <input
             type="email"
             placeholder="House, Apartment, Castle, Dome, Tiny home, Yurt..."
-            value=""
+            value={placeType}
             onChange={(event) => {
-              //   setEmail(event.target.value);
+              setPlaceType(event.target.value);
             }}
             className="w-full placeholder-slate-400 mt-5 rounded-xl h-14 bg-neutral-50 border border-solid border-slate-300 text-sm p-5 focus:outline-none focus:border-red-500 sm:max-w-[450px]"
           />
@@ -71,9 +95,10 @@ const Publish = () => {
             Your address is only shared with guests after they’ve made a
             reservation.
           </p>
-          {/* <p>lat : {coordinates.lat}</p>
+          <p>lat : {coordinates.lat}</p>
           <p>lng : {coordinates.lng}</p>
-          <p>Address : {address}</p> */}
+          <p>Address : {address}</p>
+
           <PlacesAutocomplete
             value={address}
             onChange={setAddress}
@@ -196,6 +221,128 @@ const Publish = () => {
             </div>
           </div>
           <div className="sep h-[1px] w-full bg-slate-200 my-5"></div>
+          <div className="flex justify-between items-center mt-5">
+            <p>Beds</p>
+
+            <div className="counter-basics flex items-center">
+              <button
+                className={`flex items-center justify-center w-9 h-9 rounded-full border-[1px] ${
+                  bedsCounter <= 0
+                    ? "text-slate-300 border-slate-300"
+                    : "text-slate-700 border-slate-700"
+                }`}
+                onClick={() => {
+                  if (bedsCounter > 0) {
+                    setBedsCounter(bedsCounter - 1);
+                  }
+                }}
+              >
+                -
+              </button>
+              <p
+                className={`w-14 text-center ${
+                  bedsCounter <= 0 ? "text-slate-300" : "text-slate-700"
+                }`}
+              >
+                {bedsCounter}
+              </p>
+              <button
+                className="flex items-center justify-center w-9 h-9 text-sm rounded-full border-[1px] border-slate-700"
+                onClick={() => {
+                  setBedsCounter(bedsCounter + 1);
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className="sep h-[1px] w-full bg-slate-200 my-5"></div>
+          <div className="flex justify-between items-center mt-5">
+            <p>Bathrooms</p>
+
+            <div className="counter-basics flex items-center">
+              <button
+                className={`flex items-center justify-center w-9 h-9 rounded-full border-[1px] ${
+                  bathroomsCounter <= 0
+                    ? "text-slate-300 border-slate-300"
+                    : "text-slate-700 border-slate-700"
+                }`}
+                onClick={() => {
+                  if (bathroomsCounter > 0) {
+                    setBathroomsCounter(bathroomsCounter - 1);
+                  }
+                }}
+              >
+                -
+              </button>
+              <p
+                className={`w-14 text-center ${
+                  bathroomsCounter <= 0 ? "text-slate-300" : "text-slate-700"
+                }`}
+              >
+                {bathroomsCounter}
+              </p>
+              <button
+                className="flex items-center justify-center w-9 h-9 text-sm rounded-full border-[1px] border-slate-700"
+                onClick={() => {
+                  setBathroomsCounter(bathroomsCounter + 1);
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-xl mt-10">
+            Tell guests what your place has to offer
+          </h1>
+          <p className="text-sm text-slate-400">
+            You can add more amenities after you publish your listing.
+          </p>
+          <input
+            type="email"
+            placeholder="Wifi, TV, Kitchen, Washer, Pool, BBQ grill, Fire pit..."
+            value={amenities}
+            onChange={(event) => {
+              setAmenities(event.target.value);
+            }}
+            className="w-full placeholder-slate-400 mt-5 rounded-xl h-14 bg-neutral-50 border border-solid border-slate-300 text-sm p-5 focus:outline-none focus:border-red-500 sm:max-w-[450px]"
+          />
+        </div>
+        <div>
+          <h1 className="text-xl mt-10">Ta-da! How does this look?</h1>
+          <p className="text-sm text-slate-400">
+            You can add 5 pictures maximum.
+          </p>
+          <div className="mt-5">
+            {/* <DragDropFiles multiple={true} required={true} /> */}
+            <FileUploader
+              handleChange={handleChange}
+              name="file"
+              types={fileTypes}
+              value={file}
+              multiple={true}
+              required={true}
+            />
+          </div>
+        </div>
+        <div>
+          <h1 className="text-xl mt-10">Now, let's give your house a title</h1>
+          <p className="text-sm text-slate-400">
+            Short titles work best. Have fun with it. later.
+          </p>
+
+          <textarea
+            type="email"
+            placeholder="House, Apartment, Castle, Dome, Tiny home, Yurt..."
+            value={describe}
+            onChange={handleDescribe}
+            className="w-full placeholder-slate-400 mt-5 rounded-xl h-40 bg-neutral-50 border border-solid border-slate-300 text-sm p-5 focus:outline-none focus:border-red-500 sm:max-w-[450px]"
+          />
+          <p className="text-sm">
+            {describe.length}/{maxLength}
+          </p>
         </div>
       </div>
     </>
