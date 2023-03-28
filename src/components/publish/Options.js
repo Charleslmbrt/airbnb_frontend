7bb0df5455a224534354ab83d4fe9901ff65c7fb
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2023-03-28 17:04:10 +0200

    update Options.js responsive

--- src/components/publish/Options.js
+++ src/components/publish/Options.js
@@ -1,21 +1,21 @@
 const OptionsPublish = ({ options, setOptions }) => {
   return (
     <>
-      <div>
-        <h1 className="text-xl mt-10">
+      <div className="mx-10 sm:flex sm:flex-col sm:items-center">
+        <h1 className="text-3xl font-medium">
           Tell guests what your place has to offer
         </h1>
-        <p className="text-sm text-slate-400">
+        <p className="text-xl text-light text-gray-400 my-5">
           You can add more amenities after you publish your listing.
         </p>
         <input
           type="text"
-          placeholder="Wifi, TV, Kitchen, Washer, Pool, BBQ grill, Fire pit..."
+          placeholder="Wifi, TV, Kitchen, Washer, Pool..."
           value={options}
           onChange={(event) => {
             setOptions(event.target.value);
           }}
-          className="w-full placeholder-slate-400 mt-5 rounded-xl h-14 bg-neutral-50 border border-solid border-slate-300 text-sm p-5 focus:outline-none focus:border-red-500 sm:max-w-[450px]"
+          className="text-xl w-full placeholder-slate-400 mt-5 rounded-xl h-14 bg-neutral-50 border border-solid border-slate-300 text-sm p-5 focus:outline-none focus:border-red-500 sm:max-w-[450px]"
         />
       </div>
     </>
