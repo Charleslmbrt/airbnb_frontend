089e4fa04d56c505c4e860413a581b8f9c71f7ba
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2023-03-28 16:24:03 +0200

    update Types.js repsonsive

--- src/components/publish/Types.js
+++ src/components/publish/Types.js
@@ -1,8 +1,8 @@
 const OptionsPublish = ({ type, setType }) => {
   return (
     <>
-      <div>
-        <p className="text-xl mt-10">
+      <div className="mx-10 sm:flex sm:flex-col sm:items-center">
+        <p className="text-3xl mt-10 font-medium">
           Which of these best describes your place?
         </p>
         <select
@@ -10,7 +10,7 @@ const OptionsPublish = ({ type, setType }) => {
           onChange={(event) => {
             setType(event.target.value);
           }}
-          className="classic appearance-none w-full cursor-pointer mt-5 rounded-xl bg-neutral-50 border border-solid border-slate-300 text-sm p-5 focus:outline-none focus:border-red-500 sm:max-w-[450px]"
+          className="classic appearance-none w-full text-xl cursor-pointer mt-5 rounded-xl bg-neutral-50 border border-solid border-slate-300 text-sm p-5 focus:outline-none focus:border-red-500 sm:max-w-[450px]"
         >
           <option value="">Select an option</option>
           <option value="Amazing Pool">Amazing Pool</option>
