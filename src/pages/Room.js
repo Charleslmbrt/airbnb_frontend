0d0f50ae1f35162a85ec44f56709f17f2f522c85
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2023-04-18 11:46:06 +0200

    update Room.js

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -7,7 +7,7 @@ import { Carousel } from "antd";
 
 // imports components
 import Header from "../components/Header";
-import Filters from "../components/Filters";
+// import Filters from "../components/Filters";
 
 const Rooms = ({
   userToken,
@@ -31,7 +31,7 @@ const Rooms = ({
       }
     };
     fetchDataRoom();
-  }, [setIsLoading]);
+  }, [setIsLoading, id]);
 
   const onChange = (currentSlide) => {
     console.log(currentSlide);
@@ -51,13 +51,14 @@ const Rooms = ({
           isLoading={isLoading}
           userId={userId}
         />
-        <Filters />
+        {/* <Filters /> */}
       </div>
-      <div className="mt-56">
+      <div className="mt-44">
         <Carousel afterChange={onChange}>
           {roomData?.picturesArray?.map((picture) => {
             return (
               <img
+                key={picture.asset_id}
                 src={picture.secure_url}
                 alt=""
                 className="object-cover max-h-80"
@@ -65,8 +66,36 @@ const Rooms = ({
             );
           })}
         </Carousel>
+        <div className="mx-10 mt-5">
+          <p className="text-2xl font-medium">{roomData?.title}</p>
+          <p className="text-lg mt-1">
+            {roomData?.city}, {roomData?.country}
+          </p>
 
-        <p className="text-xl">{roomData?.title}</p>
+          <p className="text-sm  text-gray-400">{roomData?.type}</p>
+          <div className="w-full my-5 h-px bg-gray-200"></div>
+          <div className="flex justify-between items-center">
+            <p className="text-2xl">Hosted by {userInfos?.firstname}</p>
+            <img
+              src={userInfos?.picture?.secure_url}
+              alt=""
+              className="w-12 h-12 rounded-full bg-red-500 object-cover"
+            />
+          </div>
+          <p className="font-light">
+            {roomData?.mainInfos.guests} guests • {roomData?.mainInfos.bedrooms}{" "}
+            bedrooms • {roomData?.mainInfos.beds} beds •
+            {roomData?.mainInfos.bathrooms} bathrooms
+          </p>
+          <div className="w-full my-5 h-px bg-gray-200"></div>
+          <p className="">{roomData?.description}</p>
+          <div className="w-full my-5 h-px bg-gray-200"></div>
+          <p className="text-xl font-medium">What this place offers</p>
+          <p className="mt-2">{roomData?.options}</p>
+          <div className="w-full my-5 h-px bg-gray-200"></div>
+          <p className="text-xl font-medium">Where you'll be</p>
+          <div className="bg-red-600 mb-[600px] w-full h-[500px]"></div>
+        </div>
       </div>
     </>
   );
